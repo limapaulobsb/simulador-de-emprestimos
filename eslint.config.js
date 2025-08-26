e91164875b4d8e7b53e307a820be315d1ed58d90
Paulo Henrique Lima <limapaulobsb@gmail.com>
2025-08-25 23:09:17 -0300

    chore: configura prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,22 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require("eslint/config");
+const expoConfig = require("eslint-config-expo/flat");
 
 module.exports = defineConfig([
   expoConfig,
   {
-    ignores: ['dist/*'],
+    ignores: ["dist/*"],
+    rules: {
+      "max-len": [
+        "warn",
+        {
+          code: 100,
+          ignoreComments: true,
+          ignoreStrings: true,
+          ignoreTemplateLiterals: true,
+          ignoreRegExpLiterals: true,
+        },
+      ],
+    },
   },
 ]);
